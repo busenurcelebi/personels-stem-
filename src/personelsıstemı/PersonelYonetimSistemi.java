65a7ad61c9ef5e4cee32a1649867f9c2d687ca5e
busenurcelebi <busenurcelebi444@gmail.com>
2023-12-26 18:31:52 +0300

    v2
    şunu bunu yaptım

--- src/personelsıstemı/PersonelYonetimSistemi.java
+++ src/personelsıstemı/PersonelYonetimSistemi.java
@@ -3,7 +3,7 @@ package personelsıstemı;
 public class PersonelYonetimSistemi {
 
 	public static void main(String[] args) {
-		 Calisan calisan = new Calisan("John", "Doe", 25, 20.0, 40);
+		 Calisan calisan = new Calisan("John", "Doe", 25, 20.0, 50);
 	
 	        calisan.bilgiGoster();
 	        Yonetici yonetici = new Yonetici("Jane", "Doe", 35, 5000.0);
